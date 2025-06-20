5ae7f49f92ec3dfcc42cc585c53df0dfe73307b2
Diptanshu <diptanshu@classyendeavors.com>
2025-06-20 22:30:28 +0530

    chore: update resources.yml for S3 bucket policy and remove unused auth handler

--- backend/src/handlers/auth.js
+++
@@ -1,9 +0,0 @@
-export const login = async (event) => {
-  console.log(event);
-  event.body = JSON.parse(event.body);
-  console.log(event.body);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello World' }),
-  };
-};
